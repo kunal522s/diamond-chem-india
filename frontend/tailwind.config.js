37f1eec2f615421fc46bfa203460244430b6a86c
Kunal Dhariwal <kunaldhariwal0007@gmail.com>
2026-07-08 21:10:40 +0530

    fgvrsdfg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -76,6 +76,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
 
