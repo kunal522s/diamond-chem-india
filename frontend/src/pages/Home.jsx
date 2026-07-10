d517d01fb2a4ee3f3ce2b0974c41ec4884e77842
Kunal Dhariwal <kunaldhariwal0007@gmail.com>
2026-07-10 16:20:08 +0530

    mobile hero update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -109,7 +109,7 @@ export default function Home() {
 
           {/* Mobile Image */}
           <img
-            src="/images/bg image mobile.png"
+            src="/images/hero6.webp"
             alt="Hero Mobile"
             className="block md:hidden h-full w-full object-cover object-center"
           />
