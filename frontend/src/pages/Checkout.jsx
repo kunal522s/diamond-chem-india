6913175eb3951a2d0d558108433382c96b2433f0
Kunal Dhariwal <kunaldhariwal0007@gmail.com>
2026-07-23 13:37:43 +0530

    download invoice new update

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useCart } from "@/context/CartContext";
-import { api } from "@/lib/api";
+import { api, API } from "@/lib/api";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import CartDrawer from "@/components/CartDrawer";
